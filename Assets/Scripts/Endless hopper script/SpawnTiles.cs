c16c102f2c11703c4207bfa71f99cb2d846e4d57
spindlyjohnny <saifulfarhan05@gmail.com>
2024-05-01 21:14:41 +0800

    spawn multiple tiles

--- Assets/Scripts/Endless hopper script/SpawnTiles.cs
+++ Assets/Scripts/Endless hopper script/SpawnTiles.cs
@@ -10,10 +10,12 @@ public class SpawnTiles : MonoBehaviour
     [SerializeField]GameObject NPC;
     [SerializeField]GameObject[]bus_stop,buildings;
     GameObject go;
+    int shiftfactor;
     // Start is called before the first frame update
     void Start()
     {
         NPC = GetComponentInChildren<NPC>(true).gameObject;
+        shiftfactor = 0;
         //Destroy(Instantiate(tile, spawnpt.position, spawnpt.rotation), 2f);
         //for (int i = 0; i < 10; i++) {
         //    Instantiate(tile, spawnpt.position, spawnpt.rotation);
@@ -23,19 +25,26 @@ public class SpawnTiles : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        SpawnTiles[] currenttiles = FindObjectsOfType<SpawnTiles>();
+        shiftfactor = currenttiles.Length == 1 ? 0 : 21; // shiftfactor spawns tiles 21 units ahead because when player enters trigger, there are 3 tiles in front. each tile is 7 units long on the x-axis
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        //SpawnTiles[] currenttiles = FindObjectsOfType<SpawnTiles>();
+        //print(currenttiles.Length);
+        //if(currenttiles.Length % 4 == 0) {
+        //    shiftfactor = 21;
+        //}
         //spawn tiles at designated area by either calling the manager or spawning the tiles itself
         if (other.GetComponent<Player>())//only the player collision will spawn the tile
         {
-            for (int x = 0; x < 4; x++) {
-                go = Instantiate(tile, spawnpt.position + new Vector3(7 * x, 0, 0), spawnpt.rotation);
+            for (int x = 0; x < 4; x++) { // spawn 4 tiles at a time
+                // spawn tile at spawn point + size of tile * order that tile was spawned
+                go = Instantiate(tile, spawnpt.position + new Vector3(7 * x, 0, 0) + new Vector3(shiftfactor,0,0), spawnpt.rotation);
                 SpawnTiles mytile = go.GetComponent<SpawnTiles>();
                 float rng = Random.Range(0f, 1f);
-                print(rng);
+                //print(rng);
                 if (rng == 0) {
                     mytile.NPC.SetActive(false);
                     foreach (GameObject i in mytile.bus_stop) i.SetActive(false);
@@ -60,6 +69,6 @@ public class SpawnTiles : MonoBehaviour
     }
     private void OnTriggerExit(Collider other) {
         //NPC.transform.position = NPC.GetComponent<NPC>().startpos;
-        //Destroy(gameObject);
+        Destroy(gameObject);
     }
 }
