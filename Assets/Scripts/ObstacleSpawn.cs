b677becf287c9241046cf9e01ff56a618e140a20
JH-AFK <NGJH.22@ichat.sp.edu.sg>
2024-05-07 11:03:45 +0800

    Added in headphone obstacle

--- Assets/Scripts/ObstacleSpawn.cs
+++ Assets/Scripts/ObstacleSpawn.cs
@@ -9,7 +9,15 @@ public class ObstacleSpawn : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
+        if (this.tag == "Headphone")
+        {
+            print("yes");
+            float random = Random.Range(-3.5f, 2.5f);
+            transform.position = new Vector3(transform.position.x , transform.position.y, transform.position.z +random);
+        }
         SpawnObstacle();
+
     }
 
     // Update is called once per frame
@@ -18,9 +26,17 @@ public class ObstacleSpawn : MonoBehaviour
         //if (myobstacle == null) SpawnObstacle();
     }
     void SpawnObstacle() {
-        myobstacle = Instantiate(obstacles[Random.Range(0, obstacles.Length)], transform.position,transform.rotation);
-        myobstacle.GetComponent<Obstacle>().myspawner = this;
-        myobstacle.transform.position += myobstacle.GetComponent<Obstacle>().spawnoffset;
+        myobstacle = Instantiate(obstacles[Random.Range(0, obstacles.Length)], transform.position,transform.rotation);        
+        if (this.tag=="Headphone") 
+        {
+            myobstacle.GetComponent<HeadphoneObstacle>().myspawner = this;
+        }
+        else
+        {
+            myobstacle.GetComponent<Obstacle>().myspawner = this;
+            myobstacle.transform.position += myobstacle.GetComponent<Obstacle>().spawnoffset;
+        }
+        
         if (myobstacle.GetComponent<Collectible>() == null) myobstacle.transform.rotation = Quaternion.Euler(0, -90, 0);
     }
 }
---
+++ Assets/Scripts/Puzzler script/JH folder/HeadphoneObstacle.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadphoneObstacle : MonoBehaviour
+{
+    public ObstacleSpawn myspawner;
+    public Vector3 spawnoffset;
+    public float distanceBeforeTurningBack=10f;
+    Vector3 InitalPosition;    
+    float distanceBetween;
+    public bool walkingRight = true;
+    Vector3 direction;
+    private void Start()
+    {
+        InitalPosition = transform.position;
+    }
+
+    protected virtual void Update()
+    {
+        float distanceBetween = Vector3.SqrMagnitude(InitalPosition - transform.position);
+        print(distanceBetween);
+        if(distanceBetween >= distanceBeforeTurningBack * distanceBeforeTurningBack && walkingRight==true)
+        {
+            walkingRight = false;
+        }
+        else if(distanceBetween <=1*1 && walkingRight==false)
+        {
+            walkingRight = true;
+        }
+
+        if (walkingRight)
+        {
+            direction = ((transform.position + Vector3.forward) - transform.position).normalized;
+            var targettedRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targettedRotation, 360*Time.deltaTime );
+            transform.Translate(3 * Time.deltaTime * Vector3.forward,Space.World);
+        }
+        else
+        {
+            direction = (InitalPosition - transform.position).normalized;
+            var targettedRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targettedRotation, 360 *Time.deltaTime);
+            transform.Translate(3 * Time.deltaTime * -Vector3.forward,Space.World);
+            
+        }
+        /*
+        if (myspawner == null) Destroy(gameObject);
+        else Destroy(gameObject, 15);*/
+    }
+}
