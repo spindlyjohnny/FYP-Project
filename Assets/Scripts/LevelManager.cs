ff49fcc5a612fed6a96f77340d843a08d746259e
spindlyjohnny <saifulfarhan05@gmail.com>
2024-05-20 13:22:35 +0800

    new train just dropped

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -37,14 +37,15 @@ public class LevelManager : SceneLoader {
 
     // Update is called once per frame
     void Update() {
-        if (npcmanager.myNPC) {
-            foreach (var i in tiles) {
-                if (i.GetComponent<RoadTile>()) tileindex = Array.IndexOf(tiles, i);
-            }
-        } 
-        else {
-            tileindex = UnityEngine.Random.Range(0, tiles.Length);
-        }
+        tileindex = UnityEngine.Random.Range(0, tiles.Length);
+        //if (npcmanager.myNPC) {
+        //    foreach (var i in tiles) {
+        //        if (i.GetComponent<RoadTile>()) tileindex = Array.IndexOf(tiles, i);
+        //    }
+        //} 
+        //else {
+        //    tileindex = UnityEngine.Random.Range(0, tiles.Length);
+        //}
         if (gameover) gameoverscreen.SetActive(true);
         scoretext.text = "Score:" + score;
         if (npcmanager.myNPC != null) {
