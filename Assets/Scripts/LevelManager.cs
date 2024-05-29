8a47f1ec623e70476230f0552e7d25dfc4bace6e
spindlyjohnny <saifulfarhan05@gmail.com>
2024-05-29 21:17:40 +0800

    wheelchair user

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -8,10 +8,10 @@ using System;
 
 public class LevelManager : SceneLoader {
     public bool gameover;
-    public GameObject gameoverscreen, taskcompletescreen,loadingscreen;
+    public GameObject gameoverscreen, taskcompletescreen, loadingscreen;
     public Slider energyslider;
-    public GameObject[] tiles; 
-    [SerializeField]GameObject[] bustiles;
+    public GameObject[] tiles;
+    [SerializeField] GameObject[] bustiles;
     public GameObject mrt;
     public int score, tileindex;
     public TMP_Text scoretext, tasksuccesstext;
@@ -21,23 +21,23 @@ public class LevelManager : SceneLoader {
     public TMP_Text dialoguetext, questiontext, explaintext, optionAtext, optionBtext, optionCtext, optionDtext;
     public TMP_Text nametext;
     public GameObject optionAButton, optionBButton, optionCButton, optionDButton;
-    public GameObject dialoguebox, questionbox;
-    int numberOfTiles=5;
+    public GameObject dialoguebox, questionbox, busstart;
+    int numberOfTiles = 5;
     public float tilerng;
-    public GameObject upgradeText;
     public enum Level { Bus, MRT };
     public Level level;
     public Tile[] currenttiles;
     CameraController cam;
     public Sprite[] loadingimgs;
     Player player;
+    public GameObject upgradeText;
     // Start is called before the first frame update
     void Start() {
         StartCoroutine(AudioManager.instance.SwitchMusic(AudioManager.instance.levelmusic));
         score = 0;
         tileshiftfactor = 0;
         npcmanager = FindObjectOfType<NPCManagement>();
-        for(int i = 0; i < tiles.Length; i++) {
+        for (int i = 0; i < tiles.Length; i++) {
             tiles[i] = bustiles[i];
         }
         //tileindex = UnityEngine.Random.Range(0, tiles.Length);
@@ -52,7 +52,7 @@ public class LevelManager : SceneLoader {
     // Update is called once per frame
     void Update() {
         //tilerng = UnityEngine.Random.Range(0f, 1f);
-        if(level == Level.Bus)RandomTile();
+        if (level == Level.Bus) RandomTile();
         else {
             for (int i = 0; i < tiles.Length; i++) {
                 tiles[i] = mrt;
@@ -64,27 +64,23 @@ public class LevelManager : SceneLoader {
         if (npcmanager.myNPC != null) {
             if (npcmanager.myNPC.tasksuccess == NPC.Task.Fail) {
                 tasksuccesstext.text = "Task failed!";
-            } 
-            else if (npcmanager.myNPC.tasksuccess == NPC.Task.Success) {
+            } else if (npcmanager.myNPC.tasksuccess == NPC.Task.Success) {
                 tasksuccesstext.text = "Task success!";
             }
         }
         if (taskcompletescreen.activeSelf) StartCoroutine(DisableTaskScreen());
         currenttiles = FindObjectsOfType<Tile>();
-        if(currenttiles.Length == 1) {
+        if (currenttiles.Length == 1) {
             tileshiftfactor = 0;
-        } 
-        else {
-            if(level == Level.Bus) {
+        } else {
+            if (level == Level.Bus) {
                 tileshiftfactor = 21;
-            } 
-            else {
+            } else {
                 tileshiftfactor = 0;
                 //tileshiftfactor = Mathf.RoundToInt(26.5f * 3);
             }
         }
-        if (currenttiles.Length > 10)
-        {
+        if (currenttiles.Length > 10) {
             Destroy(currenttiles[currenttiles.Length - 1].gameObject);
             player.inputtext.SetActive(false);
         }
@@ -109,13 +105,10 @@ public class LevelManager : SceneLoader {
         numberOfTiles = 5;
         Spawn(8);
         StartCoroutine(DisableLoadingScreen(2f));
-        foreach (var i in FindObjectsOfType<Tile>())
-        {
-            if (!i.gameObject.CompareTag("Train") && !i.gameObject.CompareTag("Bus"))
-            {
+        foreach (var i in FindObjectsOfType<Tile>()) {
+            if (!i.gameObject.CompareTag("Train") && i.gameObject != busstart) {
                 Destroy(i.gameObject);
-            }
-            else if(i.gameObject.CompareTag("Train") && i.GetComponent<TrainTile>()){
+            } else if (i.gameObject.CompareTag("Train") && i.GetComponent<TrainTile>()) {
                 player.transform.position = i.transform.Find("Player Start Point").position;
             }
         }
@@ -125,6 +118,7 @@ public class LevelManager : SceneLoader {
     public void MoveToBus() {
 
         print("yuh");
+        busstart.SetActive(true);
         loadingscreen.SetActive(true);
         loadingscreen.GetComponent<Image>().sprite = loadingimgs[UnityEngine.Random.Range(0, loadingimgs.Length)];
         for (int i = 0; i < tiles.Length; i++) {
@@ -134,10 +128,9 @@ public class LevelManager : SceneLoader {
         Spawn(8);
         StartCoroutine(DisableLoadingScreen(2f));
         foreach (var i in FindObjectsOfType<Tile>()) {
-            if (i.gameObject.CompareTag("Train") && !i.GetComponent<RoadTile>() && !i.CompareTag("Bus")) {
+            if (i.gameObject.CompareTag("Train") && !i.GetComponent<RoadTile>() && i.gameObject != busstart) {
                 Destroy(i.gameObject);
-            } 
-            else if (i.name == "Bus Start") {
+            } else if (i.gameObject == busstart) {
                 player.transform.position = i.transform.Find("Player Start Point").position;
             }
         }
@@ -152,10 +145,9 @@ public class LevelManager : SceneLoader {
             // spawn tile at spawn point + size of tile * order that tile was spawned
             Tile mytile = tiles[tileindex].GetComponent<Tile>();
             //print(mytile.spawnpt.position);
-            if(level == Level.Bus) {
+            if (level == Level.Bus) {
                 Instantiate(tiles[tileindex], mytile.spawnpt.position + new Vector3(7 * x, 0, 0) + new Vector3(tileshiftfactor, 0, 0), Quaternion.identity);
-            } 
-            else {
+            } else {
                 Instantiate(tiles[tileindex], mytile.spawnpt.position + new Vector3(26.5f * x, 0, 0) + new Vector3(tileshiftfactor, 0, 0), Quaternion.identity);
             }
             //print("Yes");
@@ -178,8 +170,7 @@ public class LevelManager : SceneLoader {
         }
     }
 
-    public void Destruction(GameObject tile)
-    {
+    public void Destruction(GameObject tile) {
 
     }
 }
