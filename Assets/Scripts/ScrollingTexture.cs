27e474ea0af2f5c60d26d7e2bafc43725e6b61e1
spindlyjohnny <saifulfarhan05@gmail.com>
2024-08-12 21:00:26 +0800

    push first

---
+++ Assets/Scripts/ScrollingTexture.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingTexture : MonoBehaviour {
+	public Vector2 uvSpeed = new Vector2(0.0f, 1.0f);
+	public Vector2 uvOffset = Vector2.zero;
+
+	void LateUpdate() {
+		uvOffset += (uvSpeed * Time.deltaTime);
+		GetComponent<MeshRenderer>().materials[0].SetTextureOffset("_BaseMap", uvOffset);
+	}
+}
--- Assets/Scripts/Video.cs
+++ Assets/Scripts/Video.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
+using UnityEngine.SceneManagement;
 
 public class Video : MonoBehaviour {
     [SerializeField] string videoName;
@@ -9,6 +10,7 @@ public class Video : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         videoPlayer = GetComponent<VideoPlayer>();
+        if (SceneManager.GetActiveScene().buildIndex == 0) PlayVideo();
     }
 
     // Update is called once per frame
