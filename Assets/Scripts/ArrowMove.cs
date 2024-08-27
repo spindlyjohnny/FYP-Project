4486a4be4651ff992f60fb5ba09fc96a4e8c6401
spindlyjohnny <saifulfarhan05@gmail.com>
2024-08-27 22:11:18 +0800

    arrow + some mat stuff

---
+++ Assets/Scripts/ArrowMove.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowMove : MonoBehaviour
+{
+    Vector3 originalpos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalpos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, Mathf.Lerp(originalpos.y,originalpos.y + 1f,Mathf.PingPong(Time.time,1)), transform.position.z);
+    }
+}
